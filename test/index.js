2995b42e36ad0ffc043710abbf228ae5f6aba9be
Miguel Angel <miguel.galiciatorrez@gmail.com>
2016-03-24 00:39:53 -0600

    File Makefile was created to add test-unit

--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
 var expect = require('chai').expect
-var Worker = require('../lib/worker')
 
 describe('this is the first test', function () {
   it('Full name', function (done) {
-    var mike = new Worker('Miguel', 'Angel')
-    expect(mike.toString()).to.be.equal('Miguel Angel')
+    expect('hello world').to.be.equal('hello world')
     done()
   })
 })
